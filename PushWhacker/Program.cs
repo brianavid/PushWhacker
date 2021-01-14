0216730e1c2edd2ab76a50726f703f5d1e9b0e48
Brian Collins <collins.bj@gmail.com>
2021-01-14 14:12:54 +0000

    Clear all lights on session ending (e.g. restart, logout)

--- PushWhacker/Program.cs
+++ PushWhacker/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows.Forms;
 
 namespace PushWhacker
 {
     static class Program
     {
+        static MidiProcessor midiProcessor;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -20,13 +23,15 @@ namespace PushWhacker
             }
             else
             {
-                MidiProcessor midiProcessor = new MidiProcessor(configValues);
+                midiProcessor = new MidiProcessor(configValues);
+
                 if (!midiProcessor.StartProcessing())
                 {
                     MessageBox.Show("Can't start Push Midi Processing");
                 }
                 else
                 {
+                    SystemEvents.SessionEnding += new SessionEndingEventHandler(PushWacker_SesssionEndingEventHandler);
                     var applicationContext = new CustomApplicationContext(configValues, midiProcessor);
                     Application.Run(applicationContext);
 
@@ -35,6 +40,12 @@ namespace PushWhacker
 
                 PushDisplay.Close();
             }
+        }
+
+        static void PushWacker_SesssionEndingEventHandler(object sender, EventArgs e)
+        {
+            midiProcessor.StopProcessing();
+            PushDisplay.Close();
         }
 
     }
